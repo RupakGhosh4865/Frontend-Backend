cc13b3f06ad5a34db84c95cb057d80b137872ce9
RupakGhosh4865 <rupak4865@gmail.com>
2024-09-11 00:22:25 +0530

    a

--- src/helpers/axios.helper.js
+++ src/helpers/axios.helper.js
@@ -2,7 +2,7 @@ import axios from "axios";
 // import { loadProgressBar } from "axios-progress-bar";
 // import "axios-progress-bar/dist/nprogress.css";
 
-const baseURL = "https://backend-playtube.onrender.com";
+const baseURL = "https://backend-playtube.onrender.com/api/v1";
 
 export const axiosInstance = axios.create({
   baseURL,
